87c15361700997069991d48fda7c7c7a6359ac80
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2023-05-30 11:30:07 +0200

    Fix fUserSiteMap, sWorkspaceCreate (WIP, cf. TODO) and sWorkspaceDestroy

--- CK.DB.Workspace.Page/Res/fUserSiteMap.sql
+++ CK.DB.Workspace.Page/Res/fUserSiteMap.sql
@@ -7,23 +7,30 @@ returns table
 as
 return
 (
-    select w.WorkspaceId
+    with q
+    as (
+        select WorkspaceId, PageId
+        from CK.tWorkspace
+        where WorkspaceId > 0
+    )
+
+    select q.WorkspaceId
           ,rp.ResPath
           ,aa.GrantLevel
-    from CK.tWorkspace w
-    inner join CK.tWebPage wp on w.PageId = wp.PageId
-    inner join CK.tResPath rp on w.PageId = rp.ResId
+    from CK.tWebPage wp
+    inner join CK.tResPath rp on wp.PageId = rp.ResId
+    inner join q on wp.PageId = q.PageId
     inner join CK.vAclActor aa on wp.AclId = aa.AclId and aa.ActorId = @ActorId
     where aa.GrantLevel >= 16
 
-    union -- Union workspaces page with all children of the workspaces page.
+    union all -- Union workspaces page with all children of the workspaces page.
 
-    select w.WorkspaceId
-          ,rpac.ResPath
+    select q.WorkspaceId
+          ,rpac.ChildName
           ,aa.GrantLevel
-    from CK.tWorkspace w
-    inner join CK.tWebPage wp on w.PageId = wp.PageId
-    inner join CK.vResPathAllChildren rpac on w.PageId = rpac.ResId
+    from CK.vResPathAllChildren rpac
+    inner join CK.tWebPage wp on rpac.ChildId = wp.PageId
+    inner join q on rpac.ResId = q.PageId
     inner join CK.vAclActor aa on wp.AclId = aa.AclId and aa.ActorId = @ActorId
     where aa.GrantLevel >= 16
 );
