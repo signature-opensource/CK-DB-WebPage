6021ce779885fefe37b6c5e8cbfb3b72940331a5
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2023-06-05 22:52:06 +0200

    Fix CK.DB.HWorkspace dependency to Workspace (upgrade) and adds sZoneMove.

--- CK.DB.HWorkspace/Package.cs
+++ CK.DB.HWorkspace/Package.cs
@@ -4,6 +4,7 @@ namespace CK.DB.HWorkspace
 {
     [SqlPackage( Schema = "CK", ResourcePath = "Res" )]
     [Versions( "1.0.0" )]
+    [SqlObjectItem( "transform:sZoneMove" )]
     public abstract class Package : SqlPackage
     {
         void StObjConstruct( CK.DB.Workspace.Package workspace, CK.DB.HZone.Package hZone )
