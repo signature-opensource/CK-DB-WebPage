d114d6376ad581bfe1f3fcf4e49fc1eee439d827
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2023-09-28 18:22:36 +0200

    Fix HWorkspace.Page sZoneMove.tql: add check workspace and parent have page, fix update workspace page path.

--- CK.DB.HWorkspace.Page/Package.cs
+++ CK.DB.HWorkspace.Page/Package.cs
@@ -6,7 +6,7 @@ namespace CK.DB.HWorkspace.Page
     [Versions( "1.0.0" )]
     [SqlObjectItem( "transform:sZoneMove" )]
     [SqlObjectItem( "transform:sWorkspacePagePlug" )]
-    public class Package : SqlPackage
+    public abstract class Package : SqlPackage
     {
         void StObjConstruct( CK.DB.Workspace.Page.Package workspacePage, CK.DB.HWorkspace.Package hworkspace )
         {
